06872ba8e319ef51d60e20bfdb2970ba78950295
hyuk <hyuk>
2020-04-13 19:11:58 +0900

    dao方式

---
+++ dao/src/main/java/com/mybatis/dao/impl/UserDaoImpl.java
@@ -0,0 +1,24 @@
+package com.mybatis.dao.impl;
+
+import com.mybatis.dao.IUserDao;
+import com.mybatis.domain.User;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.List;
+
+public class UserDaoImpl implements IUserDao {
+    private SqlSessionFactory factory ;
+    public UserDaoImpl(SqlSessionFactory factory){
+        this.factory = factory;
+    }
+    public List<User> findAll() {
+        //1.使用工厂创建SqlSession对象
+        SqlSession session = factory.openSession();
+        //2.使用session执行查询所有方法
+        List<User> users = session.selectList("com.mybatis.dao.IUserDao.findAll");
+        session.close();
+        //返回查询结果
+        return users;
+    }
+}
